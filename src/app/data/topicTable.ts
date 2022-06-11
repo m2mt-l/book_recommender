3d190ff07af58f931a68f0e8169ce4fdebc087c0
m2mt-l <ta28ma2@gmail.com>
2022-06-11 10:20:44 +0000

    feat: topicTable

---
+++ src/app/data/topicTable.ts
@@ -0,0 +1,44 @@
+export const topicTable: {[key: string]: {[key: string]: {[key: string]: string}}} = {
+    France: {
+        child: {
+            male: 'Dinosaurs',
+            female: 'Ballet',            
+        },
+        teen: {
+            male: 'Bicycles',
+            female: 'Romance',
+        },
+        adult: {
+            male: 'Business',
+            female: 'Law',
+        },
+    },
+    Brazil: {
+        child: {
+            male: 'Karate',
+            female: 'Rainforest',
+        },
+        teen: {
+            male: 'Surfing',
+            female: 'Psychology',
+        },
+        adult: {
+            male: 'Football',
+            female: 'Jewelry'
+        },
+    },
+    Australia: {
+        child: {
+            male: 'Spiders',
+            female: 'Geology',
+        }, 
+        teen: {
+            male: 'Rugby',
+            female: 'Fencing',
+        },
+        adult: {
+            male: 'Movies',
+            female: 'Sculpture',
+        },
+    },
+}

942c967720f21af990fceb1beb9732bf750f9dfb
m2mt-l <ta28ma2@gmail.com>
2022-06-12 00:42:52 +0000

    feat: setPerson

--- src/app/person.service.ts
+++ src/app/person.service.ts
@@ -1,8 +1,29 @@
 import { Injectable } from '@angular/core';
-
+import { Observable, of, Subject } from 'rxjs';
+import { Person } from '../app/model/person'
 @Injectable({
     providedIn: 'root',
 })
 export class PersonService {
-    constructor() {}
+    personSubject?: Subject<Person>
+
+    constructor() {
+        this.personSubject = new Subject();
+    }
+
+    // Test 1
+    setStaticPerson1(): void {
+        const person: Person = new Person('male', 'France', '', 25);
+        this.personSubject?.next(person);
+    }
+
+    // Test 2
+    setStaticPerson2(): void {
+        const person: Person = new Person('female', 'Australia', '', 10);
+        this.personSubject?.next(person);
+    }
+
+    setPerson(person: Person): void {
+        this.personSubject?.next(person);
+    }
 }

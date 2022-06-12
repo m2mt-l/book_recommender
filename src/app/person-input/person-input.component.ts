84affe4649e28bde463dfc01f5d76afeee1be92f
m2mt-l <ta28ma2@gmail.com>
2022-06-12 01:31:32 +0000

    feat: subscribe personSubject

--- src/app/person-input/person-input.component.ts
+++ src/app/person-input/person-input.component.ts
@@ -1,12 +1,17 @@
 import { Component, OnInit } from '@angular/core';
-
+import { PersonService } from '../person.service';
+import { Person } from '../model/person'
 @Component({
     selector: 'app-person-input',
     templateUrl: './person-input.component.html',
     styleUrls: ['./person-input.component.css'],
 })
 export class PersonInputComponent implements OnInit {
-    constructor() {}
+    person?: Person;
+
+    constructor(private personService: PersonService) {
+        personService.personSubject?.subscribe(personSubject => this.person = personSubject);
+    }
 
     ngOnInit(): void {}
 }

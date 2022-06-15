938976144097cfaa624966e6d742e08303a5f5cb
m2mt-l <ta28ma2@gmail.com>
2022-06-15 12:32:18 +0000

    feat: forms gender, country and age

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,9 @@ import { BookListComponent } from './book-list/book-list.component';
 import { BookDetailComponent } from './book-detail/book-detail.component';
 
 import { MatButtonModule } from '@angular/material/button';
+import { MatSelectModule } from '@angular/material/select';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
     declarations: [AppComponent, PersonInputComponent, BookListComponent, BookDetailComponent],
@@ -19,6 +22,9 @@ import { MatButtonModule } from '@angular/material/button';
         AppRoutingModule,
         BrowserAnimationsModule,
         MatButtonModule,
+        MatSelectModule,
+        MatFormFieldModule,
+        MatInputModule,
     ],
     providers: [],
     bootstrap: [AppComponent],
